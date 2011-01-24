ddee5d11c7d9c27990a0a602762dc20dc40419bf
benteca@gmail.com <benteca@gmail.com@0e90248b-6422-8a67-1632-7fa3c716a765>
2011-01-24 10:47:21 +0000

    Added assignmentSummary.jspx and small changes in AssignmentBean
    git-svn-id: https://abam.googlecode.com/svn/abam_employee/trunk@16 0e90248b-6422-8a67-1632-7fa3c716a765

--- src/main/java/no/uis/portal/employee/AssignmentBean.java
+++ src/main/java/no/uis/portal/employee/AssignmentBean.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
+import javax.faces.event.ValueChangeEvent;
 
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
@@ -23,13 +24,26 @@ public class AssignmentBean implements DisposableBean {
 	private String description;
 	private String studyProgram;
 	private String institute;
-	
+	public String getNumberOfStudentsError() {
+		return numberOfStudentsError;
+	}
+
+	public void setNumberOfStudentsError(String numberOfStudentsError) {
+		this.numberOfStudentsError = numberOfStudentsError;
+	}
+
+	private String numberOfStudentsError;
 	private boolean master;
 	private boolean bachelor;
 	
 	private Logger log = Logger.getLogger(AssignmentBean.class); 
 	
 	public AssignmentBean(){
+	}
+	
+	public String getType() {
+		if(master) return "Master";
+		else return "Bachelor";
 	}
 	
 	public void listen(ActionEvent event) {
@@ -52,8 +66,34 @@ public class AssignmentBean implements DisposableBean {
 			log.debug("NumberOfStudents: "+parameterMap.get(clientId+"numberOfStudents"));
 			log.debug("type: "+parameterMap.get(clientId+"type"));
 		}
+		
+		title = (String)parameterMap.get(clientId+"title");
+		description = (String)parameterMap.get(clientId+"description");
+		supervisor = (String)parameterMap.get(clientId+"supervisor");
+		facultySupervisor = (String)parameterMap.get(clientId+"facultySupervisor");
+		institute = (String)parameterMap.get(clientId+"institute");
+		studyProgram = (String)parameterMap.get(clientId+"studyProgram");
+		numberOfStudents = (String)parameterMap.get(clientId+"numberOfStudents");
+		numberOfStudentsError = "";
+		
+		if(parameterMap.get(clientId+"type").equals("master")){
+			master = true;
+			bachelor = false;
+		} else {
+			master = false;
+			bachelor = true;
+		}
+		
+		if(parameterMap.get(clientId+"type").equals("master")){
+			if(!parameterMap.get(clientId+"numberOfStudents").equals("1")){
+				numberOfStudentsError = "Maximum number of students on a master assignment is 1.";
+				numberOfStudents = "1";
+			}
+		}
+		
+		
 	}
-	
+
 	public void dispose() throws Exception {
 	}
 
