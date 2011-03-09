507f10ab03d9fd1d5532d1bcca29e6d58c917816
benteca@gmail.com <benteca@gmail.com@0e90248b-6422-8a67-1632-7fa3c716a765>
2011-03-09 11:54:59 +0000

    Added ThesisInformation class.
    git-svn-id: https://abam.googlecode.com/svn/abam_dom/trunk@159 0e90248b-6422-8a67-1632-7fa3c716a765

---
+++ src/main/java/no/uis/abam/dom/ThesisInformation.java
@@ -0,0 +1,72 @@
+package no.uis.abam.dom;
+
+public class ThesisInformation {
+	
+	private boolean selected;
+		
+	private String assignmentTitle;
+	private String studentName;
+	private String coStudent1Name;
+	private String coStudent2Name;
+	private String externalExaminerName;
+		
+	private Thesis thesis;
+	
+	public ThesisInformation() {}
+
+	public boolean isSelected() {
+		return selected;
+	}
+
+	public void setSelected(boolean selected) {
+		this.selected = selected;
+	}
+	
+	public String getAssignmentTitle() {
+		return assignmentTitle;
+	}
+
+	public void setAssignmentTitle(String assignmentTitle) {
+		this.assignmentTitle = assignmentTitle;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public String getCoStudent1Name() {
+		return coStudent1Name;
+	}
+
+	public void setCoStudent1Name(String coStudent1Name) {
+		this.coStudent1Name = coStudent1Name;
+	}
+
+	public String getCoStudent2Name() {
+		return coStudent2Name;
+	}
+
+	public void setCoStudent2Name(String coStudent2Name) {
+		this.coStudent2Name = coStudent2Name;
+	}
+
+	public Thesis getThesis() {
+		return thesis;
+	}
+
+	public void setThesis(Thesis thesis) {
+		this.thesis = thesis;
+	}
+
+	public String getExternalExaminerName() {
+		return externalExaminerName;
+	}
+
+	public void setExternalExaminerName(String externalExaminerName) {
+		this.externalExaminerName = externalExaminerName;
+	}
+}
