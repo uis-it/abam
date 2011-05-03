536fe14aa881b309a43d4e008af7032d6b7234c6
benteca@gmail.com <benteca@gmail.com@0e90248b-6422-8a67-1632-7fa3c716a765>
2011-05-03 10:20:56 +0000

    Prepared to get employees from LDAP
    git-svn-id: https://abam.googlecode.com/svn/abam_dom/trunk@323 0e90248b-6422-8a67-1632-7fa3c716a765

--- src/main/java/no/uis/abam/dom/Employee.java
+++ src/main/java/no/uis/abam/dom/Employee.java
@@ -1,8 +1,11 @@
 package no.uis.abam.dom;
 
+import java.util.List;
+
 public class Employee extends Person{
 
 	private String employeeId;
+	private List<Group> groupMembership;
 	
 	public Employee() {
 		
@@ -19,4 +22,13 @@ public class Employee extends Person{
 	public boolean equals(Employee employee) {
 		return this.getEmployeeId().equals(employee.getEmployeeId());
 	}
+
+	public List<Group> getGroupMembership() {
+		return groupMembership;
+	}
+
+	public void setGroupMembership(List<Group> groupMembership) {
+		this.groupMembership = groupMembership;
+	}
+	
 }
---
+++ src/main/java/no/uis/abam/dom/Group.java
@@ -0,0 +1,46 @@
+package no.uis.abam.dom;
+
+public class Group {
+
+	private String name;
+	private String organization;
+	private String orgUnit;
+	private String description;
+	
+	public Group() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getOrganization() {
+		return organization;
+	}
+
+	public void setOrganization(String organization) {
+		this.organization = organization;
+	}
+
+	public String getOrgUnit() {
+		return orgUnit;
+	}
+
+	public void setOrgUnit(String orgUnit) {
+		this.orgUnit = orgUnit;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
