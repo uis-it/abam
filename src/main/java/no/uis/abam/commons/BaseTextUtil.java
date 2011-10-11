d4b059d388085c20991b5a06d1045bade099472e
mgoldhahn@gmail.com <mgoldhahn@gmail.com@0e90248b-6422-8a67-1632-7fa3c716a765>
2011-10-11 14:46:11 +0000

    - LazyLoad util (based on SOJO) - employee basically working - student portlet contains still some issues
    git-svn-id: https://abam.googlecode.com/svn/abam_commons/trunk@376 0e90248b-6422-8a67-1632-7fa3c716a765

---
+++ src/main/java/no/uis/abam/commons/BaseTextUtil.java
@@ -0,0 +1,22 @@
+package no.uis.abam.commons;
+
+import java.util.List;
+
+import no.uis.service.model.BaseText;
+
+public final class BaseTextUtil {
+
+  public static String getText(List<BaseText> txtList, String lang) {
+    for (BaseText txt : txtList) {
+      if (txt.getLang().equals(lang)) {
+        return txt.getValue();
+      }
+    }
+    return txtList.get(0).getValue();
+  }
+
+  
+  
+  // not instantiable
+  private BaseTextUtil() {}
+}
