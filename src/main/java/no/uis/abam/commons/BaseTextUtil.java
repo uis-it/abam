6ce2b7762e8df1ffea50e7c0f6cf5a521f39b4e2
mgoldhahn@gmail.com <mgoldhahn@gmail.com@0e90248b-6422-8a67-1632-7fa3c716a765>
2011-10-24 11:57:10 +0000

    git-svn-id: https://abam.googlecode.com/svn/abam_commons/trunk@381 0e90248b-6422-8a67-1632-7fa3c716a765

--- src/main/java/no/uis/abam/commons/BaseTextUtil.java
+++ src/main/java/no/uis/abam/commons/BaseTextUtil.java
@@ -7,9 +7,11 @@ import no.uis.service.model.BaseText;
 public final class BaseTextUtil {
 
   public static String getText(List<BaseText> txtList, String lang) {
-    for (BaseText txt : txtList) {
-      if (txt.getLang().equals(lang)) {
-        return txt.getValue();
+    if (lang != null) {
+      for (BaseText txt : txtList) {
+        if (txt.getLang().equals(lang)) {
+          return txt.getValue();
+        }
       }
     }
     return txtList.get(0).getValue();
