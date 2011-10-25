f6d134d2abe268feb5a41cf21ffcb935d56bf52a
mgoldhahn@gmail.com <mgoldhahn@gmail.com@0e90248b-6422-8a67-1632-7fa3c716a765>
2011-10-25 12:42:43 +0000

    improved attachment handling
    git-svn-id: https://abam.googlecode.com/svn/abam_dom/trunk@382 0e90248b-6422-8a67-1632-7fa3c716a765

--- src/main/java/no/uis/abam/dom/Attachment.java
+++ src/main/java/no/uis/abam/dom/Attachment.java
@@ -1,20 +1,11 @@
 package no.uis.abam.dom;
 
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.Lob;
-import javax.persistence.Transient;
-
-import org.apache.commons.io.IOUtils;
 
 @Entity(name="Attachment")
 @Inheritance(strategy=InheritanceType.JOINED)
@@ -22,56 +13,38 @@ public class Attachment extends AbamType {
 
   private static final long serialVersionUID = 1L;
 
-  @Transient
-  private File file;
-  
   @Basic(fetch=FetchType.LAZY)
   @Lob
   private byte[] data;
   
+  private String fileName;
+  
+  private String contentType;
+  
   public Attachment() {
   }
   
-  public Attachment(String physicalPath) {
-    file = new File(physicalPath);
-  }
-
-  public File getFile() {
-    if (file == null && data != null) {
-      File tmpFile;
-      FileOutputStream fos = null;
-      try {
-        tmpFile = File.createTempFile("abam", ".tmp");
-        fos = new FileOutputStream(tmpFile);
-        IOUtils.write(data, fos);
-        file = tmpFile;
-      } catch(IOException e) {
-      } finally {
-        IOUtils.closeQuietly(fos);
-      }
-    }
-    return file;
-  }
-
-  public void setFile(File file) {
-    this.file = file;
-  }
-
   public byte[] getData() {
-    if (data == null && file != null && file.exists()) {
-      try {
-        FileInputStream fi = new FileInputStream(file);
-        ByteArrayOutputStream bo = new ByteArrayOutputStream();
-        IOUtils.copyLarge(fi, bo);
-        data = bo.toByteArray();
-      } catch(Exception e) {
-        throw new RuntimeException(e);
-      }
-    }
     return data;
   }
 
   public void setData(byte[] data) {
     this.data = data;
+  }
+
+  public String getFileName() {
+    return fileName;
+  }
+
+  public void setFileName(String fileName) {
+    this.fileName = fileName;
+  }
+
+  public String getContentType() {
+    return contentType;
+  }
+
+  public void setContentType(String contentType) {
+    this.contentType = contentType;
   }
 }
