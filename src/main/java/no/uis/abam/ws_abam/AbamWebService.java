4a036e7e9d3e7e4fc7edd04e3b5ccfbc94435fce
mgoldhahn@gmail.com <mgoldhahn@gmail.com@0e90248b-6422-8a67-1632-7fa3c716a765>
2012-01-18 11:50:47 +0000

    added @WebParam to Webservice
    git-svn-id: https://abam.googlecode.com/svn/ws-abam/trunk@403 0e90248b-6422-8a67-1632-7fa3c716a765

--- src/main/java/no/uis/abam/ws_abam/AbamWebService.java
+++ src/main/java/no/uis/abam/ws_abam/AbamWebService.java
@@ -3,6 +3,7 @@ package no.uis.abam.ws_abam;
 import java.util.List;
 import java.util.TreeSet;
 
+import javax.jws.WebParam;
 import javax.jws.WebService;
 
 import no.uis.abam.dom.*;
@@ -32,24 +33,24 @@ public interface AbamWebService {
 	/**
 	 * @param assignment - Assignment that should be saved
 	 */
-	public void saveAssignment(Assignment assignment);
+	public void saveAssignment(@WebParam(name="assignment") Assignment assignment);
 	
 	/**
 	 * @param assignment - Assignment that should be removed
 	 */
-	public void removeAssignment(Assignment assignment);
+	public void removeAssignment(@WebParam(name="assignment") Assignment assignment);
 	
 	/**
 	 * @param studentNumber - Student to get custom Assignment from
 	 * @return the custom Assignment of the given Student
 	 */
-	public Assignment getCustomAssignmentFromStudentNumber(String studentNumber);
+	public Assignment getCustomAssignmentFromStudentNumber(@WebParam(name="studentNumber") String studentNumber);
 	
 	/**
 	 * @param id to find Assignment by
 	 * @return Assignment with given id or null if not found
 	 */
-	public Assignment getAssignmentFromId(long id);
+	public Assignment getAssignmentFromId(@WebParam(name="assignmentId") long id);
 	
 	/**
 	 * @return a List containing Department objects
@@ -65,23 +66,23 @@ public interface AbamWebService {
 	 * @param code of the department 
 	 * @return A list with all master Applications on the given department
 	 */
-	public List<Application> getMasterApplicationListFromDepartmentCode(String code);
+	public List<Application> getMasterApplicationListFromDepartmentCode(@WebParam(name="departmentCode") String code);
 	
 	/**
 	 * @param code of the department
 	 * @return A list with all bachelor Applications on the given department
 	 */
-	public List<Application> getBachelorApplicationListFromDepartmentCode(String code);
+	public List<Application> getBachelorApplicationListFromDepartmentCode(@WebParam(name="departmentCode") String code);
 	
 	/**
 	 * @param application object that will be saved
 	 */
-	public void saveApplication(Application application);
+	public void saveApplication(@WebParam(name="application") Application application);
 	
 	/**
 	 * @param application object that will be removed
 	 */
-	public void removeApplication(Application application);
+	public void removeApplication(@WebParam(name="application") Application application);
 	
 	/**
 	 * @return next available id, assignmentList size plus one
@@ -91,7 +92,7 @@ public interface AbamWebService {
 	/**
 	 * @param thesesToAdd a List of Thesis object that should be added
 	 */
-	public void addThesesFromList(List<Thesis> thesesToAdd);
+	public void addThesesFromList(@WebParam(name="thesis") List<Thesis> thesesToAdd);
 
 	/**
 	 * @return a List with all active Thesis objects	  
@@ -102,61 +103,60 @@ public interface AbamWebService {
 	 * @param depCode of the department
 	 * @return a List with all theses of the given department
 	 */
-	public List<Thesis> getThesisListFromDepartmentCode(String depCode);
+	public List<Thesis> getThesisListFromDepartmentCode(@WebParam(name="departmentCode") String depCode);
 	
 	/**
 	 * @param thesisToUpdate Thesis object that needs to be updated
 	 */
-	public void updateThesis(Thesis thesisToUpdate);
+	public void updateThesis(@WebParam(name="thesis") Thesis thesisToUpdate);
 	
 	/**
 	 * @param departmentCode
 	 * @return A List of Thesis objects that are archived for the given department
 	 */
-	public List<Thesis> getArchivedThesisListFromDepartmentCode(String departmentCode);
+	public List<Thesis> getArchivedThesisListFromDepartmentCode(@WebParam(name="deparmentCode") String departmentCode);
 	
 	/**
 	 * @param uisLoginName - employee id
 	 * @return A List of Thesis objects that are archived for the given employee
 	 */
-	public List<Thesis> getArchivedThesisListFromUisLoginName(String uisLoginName);
+	public List<Thesis> getArchivedThesisListFromUisLoginName(@WebParam(name="loginName") String uisLoginName);
 	
 	/**
 	 * @param loginName - id of the employee 
 	 * @return Employee object if found, null if not found
 	 */
-	public Employee getEmployeeFromUisLoginName(String loginName);
+	public Employee getEmployeeFromUisLoginName(@WebParam(name="loginName") String loginName);
 	
 	/**
 	 * @param name of the employee
 	 * @return returns a Employee object for the correct employee if name is found, if not it returns a Employee object with empty name.
 	 */
 	@Deprecated
-	public Employee getEmployeeFromFullName(String name);
+	public Employee getEmployeeFromFullName(@WebParam(name="name") String name);
 
 	/**
 	 * @param tempApplicationPriorityArray a array of Applications to update
 	 */
-	public void updateApplications(
-			List<Application> tempApplicationPriorityArray);
+	public void updateApplications(@WebParam(name="application") List<Application> applicationList);
 	
 	/**
 	 * @param studentNumber
 	 * @return Student object for given studentNumber
 	 */
-	public Student getStudentFromStudentNumber(String studentNumber);
+	public Student getStudentFromStudentNumber(@WebParam(name="studentNumber") String studentNumber);
 	
 	/**
 	 * @param studentToUpdate - Student object with new information that needs to be saved in webservice
 	 * @return 
 	 */
-	public Student updateStudent(Student studentToUpdate);
+	public Student updateStudent(@WebParam(name="student") Student studentToUpdate);
 
-  public List<no.uis.service.model.StudyProgram> getStudyProgramsFromDepartmentFSCode(String departmentCode);
+  public List<no.uis.service.model.StudyProgram> getStudyProgramsFromDepartmentFSCode(@WebParam(name="departmentCode") String departmentCode);
 	
-  public no.uis.service.model.StudyProgram getStudyProgramFromCode(String programCode);
+  public no.uis.service.model.StudyProgram getStudyProgramFromCode(@WebParam(name="programCode") String programCode);
 
-  public List<AffiliationType> getAffiliation(String employeeId);
+  public List<AffiliationType> getAffiliation(@WebParam(name="employeeId") String employeeId);
 
-  public Organization getEmployeeDeptarment(String employeeId);
+  public Organization getEmployeeDeptarment(@WebParam(name="employeeId") String employeeId);
 }
