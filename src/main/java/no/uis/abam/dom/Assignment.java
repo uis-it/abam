5f1d88a3e2fc90b14c6fee3550e9603bca5faa42
mgoldhahn@gmail.com <mgoldhahn@gmail.com@0e90248b-6422-8a67-1632-7fa3c716a765>
2012-01-24 10:25:59 +0000

    removed unnecessary  code
    git-svn-id: https://abam.googlecode.com/svn/abam_dom/trunk@411 0e90248b-6422-8a67-1632-7fa3c716a765

--- src/main/java/no/uis/abam/dom/Assignment.java
+++ src/main/java/no/uis/abam/dom/Assignment.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
