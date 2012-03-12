ee796f1bfc123567bc96b80d842008b97cea64d2
mgoldhahn@gmail.com <mgoldhahn@gmail.com@0e90248b-6422-8a67-1632-7fa3c716a765>
2012-03-12 12:43:12 +0000

    put config props out of PropertyPlaceHolderConfigurer because this is not initialized lazily and fails if the portal-impl is initialized after abam-student
    git-svn-id: https://abam.googlecode.com/svn/abam_commons/trunk@483 0e90248b-6422-8a67-1632-7fa3c716a765

--- src/main/java/no/uis/abam/commons/LiferayProperties.java
+++ src/main/java/no/uis/abam/commons/LiferayProperties.java
@@ -9,26 +9,28 @@ public class LiferayProperties {
   private static final String _METHOD_GET_PROPERTIES = "getProperties";
   private static final String _CLASS = "com.liferay.portal.util.PropsUtil";
 
-  public static Properties getProperties() throws Exception {
+  private Properties properties;
+  
+  public LiferayProperties() throws Exception {
     Object returnObj = PortalClassInvoker.invoke(
       _CLASS, _METHOD_GET_PROPERTIES, false);
 
     if (returnObj != null) {
-      return (Properties)returnObj;
-    } else {
-      return null;
+      this.properties = (Properties)returnObj;
     }
   }
   
-  public static Properties getProperties(String prefix, boolean removePrefix) throws Exception {
+  public LiferayProperties(String prefix, boolean removePrefix) throws Exception {
     Object returnObj = PortalClassInvoker.invoke(
       _CLASS, _METHOD_GET_PROPERTIES, prefix, Boolean.valueOf(removePrefix), false);
     
     if (returnObj != null) {
-      return (Properties)returnObj;
-    } else {
-      return null;
+      this.properties = (Properties)returnObj;
     }
+  }
+  
+  public Properties getProperties() {
+    return this.properties;
   }
   
 }
